522f1cb1c4593337a3c5c82711a0f6b1263eeefc
ugsakshii20 <138098129+ugsakshii20@users.noreply.github.com>
2023-11-28 01:06:23 +0530

    Create usingarray.cpp

---
+++ usingarray.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+ 
+class Employee
+{
+  int id;
+  char name[30];
+  public:
+
+  void getdata();
+
+  void putdata();
+};
+ 
+void Employee::getdata()
+{ 
+  cout << "Enter Id : ";
+  cin >> id;
+  cout << "Enter Name : ";
+  cin >> name;
+}
+
+void Employee::putdata()
+{
+  cout << id << " ";
+  cout << name << " ";
+  cout << endl;
+}
+ 
+// Driver code
+int main()
+{
+
+  Employee emp[30]; 
+  int n, i;
+  cout << "Enter Number of Employees - ";
+  cin >> n;
+   
+
+  for(i = 0; i < n; i++) 
+    emp[i].getdata();
+   
+  cout << "Employee Data - " << endl;
+   
+  // Accessing the function
+  for(i = 0; i < n; i++) 
+    emp[i].putdata();
+}
